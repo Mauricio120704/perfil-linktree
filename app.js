a0c19fc06b78a7bcb50a2c6d23d089c8280f7869
Mauricio Martinez <mauricio120714@gmail.com>
2025-02-05 20:03:18 -0500

    Agregar imagenes para los botones

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let datosDOM = document.querySelector("#datos");
-datosDOM.innerHTML= `
-<h1>Mauricio Martinez Izo</h1>
-<p>20 años | Estudiante de EnterTechSchool | Lima | Perú</p>
-`
+datosDOM.innerHTML = `
+    <h1>Mauricio Martinez Izo</h1>
+    <p>20 años | Estudiante de EnterTechSchool | Lima | Perú</p>
+    `
