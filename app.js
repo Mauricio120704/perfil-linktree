7b23f76f4474df678b29797ed9a75398399d467e
Mauricio Martinez <mauricio120714@gmail.com>
2025-02-05 20:42:54 -0500

    Actualización de pagina

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let datosDOM = document.querySelector("#datos");
 datosDOM.innerHTML = `
     <h1>Mauricio Martinez Izo</h1>
-    <p>20 años | Estudiante de EnterTechSchool | Lima | Perú</p>
+    <p>20 años | Estudiante de EnterTechSchool</p>
     `
+let botonesDOM = document.querySelector(".buttons");
+botonesDOM.innerHTML =`
+<a href="https://github.com/Mauricio120704" target="_blank" class="btn">GitHub <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub-icon"></a>
+            <a href="mailto:martinezizomauricio04@gmail.com" target="_blank" class="btn">Correo Electrónico <img src="https://static.vecteezy.com/system/resources/previews/016/716/465/non_2x/gmail-icon-free-png.png" alt="Gmail-icon"></a>
+            <a href="https://wa.me/51920107243" target="_blank" class="btn">WhatsApp <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp-icon"></a>
+`
+let iconosDOM = document.querySelector(".social-icons");
+iconosDOM.innerHTML =`
+<a href="https://open.spotify.com/user/mauricio120714?si=fb6ab864f9cc4796" title="Spotify" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1200px-Spotify_logo_without_text.svg.png" alt="Spotify"></a>
+            <a href="https://www.instagram.com/mauricio.martinez07/" title="Instagram" target="_blank"><img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png" alt="Instagram"></a>
+            <a href="https://www.twitch.tv/bysk4n" title="Twitch" target="_blank"><img src="https://www.pngall.com/wp-content/uploads/13/Twitch-Logo-PNG-File-1.png" alt="Twitch"></a>
+`
